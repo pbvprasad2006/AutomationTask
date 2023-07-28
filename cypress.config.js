e342c90cd7075f0635ab130c025a0a8a25d5556c
pbvprasad2006 <140672918+pbvprasad2006@users.noreply.github.com>
2023-07-28 11:59:55 +0000

    Updates - code - 2

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern:'cypress/integration/testcases/*.js'
+    specPattern:'cypress/integration/testcases/*.js',
+    "reporter": "mochawesome",
+    "reporterOptions": {
+        "reportDir": "cypress/report/mocha-report",
+        "overwrite": false,
+        "html": false,
+        "json": true,
+        "timestamp": "mmddyyyy_HHMMss"
+    }
   },
+  
 });
